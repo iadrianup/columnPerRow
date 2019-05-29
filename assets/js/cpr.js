39a00fdfb974828d55d42cecb2cce48f955170f4
Adrián Olea Valadez <adrian@sectorweb.mx>
2019-05-28 19:04:43 -0500

    avance constructor de la clase ColumnsPerRow

--- assets/js/cpr.js
+++ assets/js/cpr.js
@@ -1,4 +1,59 @@
-class CPR {
+class ColumnsPerRow {
+	constructor(userConfig) {
+		let defaults = this.getDefaults();
+		let config = this.mergeConfig(defaults, userConfig);
+		return false;
+	}
+
+	getDefaults() {
+		const defaults = {
+			container: null, // required
+			controls: {
+				container: '.cpr-controls', // optional
+				customHtml: null, // optional
+				breakpoints: {
+					sm: [1, 2],
+					md: null,
+					lg: [3, 4],
+					xl: null
+				} // optional
+			},
+			transitions: false
+		};
+
+		return defaults;
+	}
+
+	mergeConfig(defaults, userConfig) {
+		let configMerged;
+		let configMergedControls;
+		const defaultBreakpoints = defaults.controls;
+
+		if (userConfig.hasOwnProperty('controls')) {
+			configMergedControls = Object.assign(defaults.controls, userConfig.controls);
+
+			/* if (userConfig.controls.hasOwnProperty('breakpoints')) {
+				const userBreakpoints = userConfig.controls.breakpoints;
+
+				for (const breakpoint in defaultBreakpoints) {
+					if (userBreakpoints.hasOwnProperty(breakpoint)) {
+						mergedControls[breakpoint] = userBreakpoints[breakpoint];
+					} else {
+						mergedControls[breakpoint] = defaultBreakpoints[breakpoint];
+					}
+				}
+			} else {
+				mergedControls = defaultBreakpoints;
+			} */
+		} else {
+			configMergedControls = defaultBreakpoints;
+		}
+
+		configMerged = Object.assign(defaults, userConfig);
+		configMerged.controls = configMergedControls;
+		return configMerged;
+	}
+
 	static updateGrid(control) {
 		const galleryRef = control.getAttribute('href');
 		const galleryWrapper = document.querySelector(galleryRef);
@@ -7,8 +62,8 @@ class CPR {
 			const text = galleryWrapper.classList.value;
 
 			galleryWrapper.classList.replace(
-				text.match(/cpr-[0-9]/g)[0],
-				'cpr-' + columns
+				text.match(/cpr-xs-[0-9]/g)[0],
+				'cpr-xs-' + columns
 			);
 
 			galleryWrapper.classList.replace(
@@ -25,7 +80,21 @@ if (cprControls.length) {
 	cprControls.forEach(control =>
 		control.addEventListener('click', e => {
 			e.preventDefault();
-			CPR.updateGrid(control);
+			ColumnsPerRow.updateGrid(control);
 		})
 	);
 }
+
+const cprGrid = new ColumnsPerRow({
+	container: '.cpr-gallery',
+	controls: {
+		container: '.cpr-custom-controls',
+		breakpoints: {
+			sm: [1, 2],
+			md: [2, 3],
+			lg: [3, 4],
+			xl: [4, 5]
+		}
+	},
+	transitions: true
+});
