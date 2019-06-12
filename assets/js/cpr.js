a83f2122dcdcd849d9d0ee963263b4526df5a0d7
Adrián Olea Valadez <adrian@sectorweb.mx>
2019-06-11 19:47:26 -0500

    css unused erased, file separated to init the plugin

--- assets/js/cpr.js
+++ assets/js/cpr.js
@@ -224,34 +224,3 @@ class ColumnsPerRow {
 		}
 	}
 }
-
-const cprGrid = new ColumnsPerRow({
-	container: '.cpr-gallery', // required String
-	controls: {
-		container: '.cpr-custom-controls', // String
-		customHtml: false, // String
-		breakpoints: {
-			xs: {
-				opts: [1, 2],
-				default: 1
-			},
-			sm: {
-				opts: [2, 3],
-				default: 2
-			},
-			md: {
-				opts: [3, 4],
-				default: 3
-			},
-			lg: {
-				opts: [3, 4, 5],
-				default: 3
-			},
-			xl: {
-				opts: [4, 5, 6],
-				default: 4
-			}
-		}
-	}, // optional
-	transitions: false // optional Bool
-});
