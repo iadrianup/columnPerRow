892986bdb53602ea35828466ce4ea13707574f5d
Adrián Olea Valadez <adrian@sectorweb.mx>
2019-06-11 19:03:11 -0500

    v1

--- assets/js/cpr.js
+++ assets/js/cpr.js
@@ -1,17 +1,14 @@
 class ColumnsPerRow {
 	constructor(userConfig) {
 		const defaults = this.getDefaults();
-		const config =
+		this.config =
 			typeof userConfig == 'undefined'
 				? defaults
 				: this.mergeConfig(defaults, userConfig);
 
-		this.config = config;
-		this.init();
-	}
-
-	init() {
 		this.buildControls();
+		this.buildGallery();
+		this.handleControls();
 	}
 
 	buildControls() {
@@ -21,30 +18,105 @@ class ColumnsPerRow {
 
 		if (!customHtml) {
 			const {
-				controls: { container },
+				container,
+				controls: { container: containerControls },
 				controls: { breakpoints }
 			} = this.config;
 
 			const controlsWrapper = document.createElement('ul');
-			let items = [];
-
 			controlsWrapper.classList.add('cpr-controls');
+
+			const containerEl = document.querySelector(container);
+			const containerId = containerEl.getAttribute('id');
+
+			let controls = [];
 
 			Object.keys(breakpoints).forEach(breakpoint => {
 				const breakpointType = typeof breakpoints[breakpoint];
 				if (breakpointType !== null) {
 					breakpoints[breakpoint].opts.forEach(opt => {
-						if (!items.includes(opt)) items.push(opt);
+						let control = controls.find(index => index.opt == opt);
+						if (control) {
+							control.classes += 'visible-' + breakpoint + ' ';
+						} else {
+							let newControl = {
+								opt: opt,
+								classes: 'visible-' + breakpoint + ' '
+							};
+							controls.push(newControl);
+						}
 					});
 				}
 			});
 
-			items.sort();
+			controls.sort();
 
-			const controlsHTML = items.forEach(item => {});
+			let controlsHTML = '<li>Ver:</li>';
 
-			/* <a href="#cpr_gallery" class="cpr-control" role="button" title="1 columns per row"
-                                    aria-controls="cpr_controls" data-cpr="1">1</a> */
+			controls.forEach(control => {
+				let classes = control.classes.replace(/\s+$/, '');
+				controlsHTML += `
+                <li class="${classes}"><a href="#${containerId}" class="cpr-control" role="button" title="${
+					control.opt
+				} columns per row" aria-controls="${containerId}" data-cpr="${
+					control.opt
+				}">${control.opt}</a></li>`;
+			});
+
+			controlsWrapper.innerHTML = controlsHTML;
+
+			let parentEl;
+			if (containerControls !== null) {
+				parentEl = document.querySelector(containerControls);
+				parentEl.prepend(controlsWrapper);
+			} else {
+				parentEl = containerEl.parentNode;
+				parentEl.insertBefore(controlsWrapper, containerEl);
+			}
+		}
+	}
+
+	buildGallery() {
+		const {
+			container,
+			controls: { breakpoints },
+			transitions
+		} = this.config;
+
+		const containerEl = document.querySelector(container);
+		containerEl.classList.add('cpr-grid');
+
+		Object.keys(breakpoints).forEach(breakpoint => {
+			const breakpointType = typeof breakpoints[breakpoint];
+			if (breakpointType !== null) {
+				containerEl.classList.add(
+					'cpr-' + breakpoint + '-' + breakpoints[breakpoint].default
+				);
+			}
+		});
+
+		if (transitions) {
+			containerEl.classList.add('cpr-transitions');
+		}
+	}
+
+	handleControls() {
+		const { container } = this.config;
+		const containerId = document.querySelector(container).getAttribute('id');
+		const cprControls = document.querySelectorAll(
+			'.cpr-control[aria-controls="' + containerId + '"]'
+		);
+
+		if (cprControls.length) {
+			cprControls.forEach(control =>
+				control.addEventListener('click', e => {
+					e.preventDefault();
+					ColumnsPerRow.updateGrid(
+						control,
+						ColumnsPerRow.getCurrentBreakpoint()
+					);
+				})
+			);
 		}
 	}
 
@@ -101,7 +173,42 @@ class ColumnsPerRow {
 		return configMerged;
 	}
 
-	static updateGrid(control) {
+	static getCurrentBreakpoint() {
+		let currentSize = window.screen.width,
+			xs = getComputedStyle(document.documentElement).getPropertyValue('--cpr-xs'),
+			sm = getComputedStyle(document.documentElement).getPropertyValue('--cpr-sm'),
+			md = getComputedStyle(document.documentElement).getPropertyValue('--cpr-md'),
+			lg = getComputedStyle(document.documentElement).getPropertyValue('--cpr-lg'),
+			xl = getComputedStyle(document.documentElement).getPropertyValue('--cpr-xl');
+
+		if (
+			window.matchMedia('(min-width: ' + xs + ') and (max-width: ' + sm + ')')
+				.matches
+		) {
+			currentSize = 'xs';
+		} else if (
+			window.matchMedia('(min-width: ' + sm + ') and (max-width: ' + md + ')')
+				.matches
+		) {
+			currentSize = 'sm';
+		} else if (
+			window.matchMedia('(min-width: ' + md + ') and (max-width: ' + lg + ')')
+				.matches
+		) {
+			currentSize = 'md';
+		} else if (
+			window.matchMedia('(min-width: ' + lg + ') and (max-width: ' + xl + ')')
+				.matches
+		) {
+			currentSize = 'lg';
+		} else if (window.matchMedia('(min-width: ' + xl + ')').matches) {
+			currentSize = 'xl';
+		}
+
+		return currentSize;
+	}
+
+	static updateGrid(control, breakpoint) {
 		const galleryRef = control.getAttribute('href');
 		const galleryWrapper = document.querySelector(galleryRef);
 		if (document.body.contains(galleryWrapper)) {
@@ -109,41 +216,27 @@ class ColumnsPerRow {
 			const text = galleryWrapper.classList.value;
 
 			galleryWrapper.classList.replace(
-				text.match(/cpr-xs-[0-9]/g)[0],
-				'cpr-xs-' + columns
-			);
-
-			galleryWrapper.classList.replace(
-				text.match(/cpr-lg-[0-9]/g)[0],
-				'cpr-lg-' + columns
+				text.match(
+					new RegExp('cpr[-](' + breakpoint + ')[-](0?[1-9]|1[012])')
+				)[0],
+				'cpr-' + breakpoint + '-' + columns
 			);
 		}
 	}
 }
 
-const cprControls = document.querySelectorAll('.cpr-control');
-
-if (cprControls.length) {
-	cprControls.forEach(control =>
-		control.addEventListener('click', e => {
-			e.preventDefault();
-			ColumnsPerRow.updateGrid(control);
-		})
-	);
-}
-
 const cprGrid = new ColumnsPerRow({
-	container: '.cpr-gallery', // required
+	container: '.cpr-gallery', // required String
 	controls: {
-		container: '.cpr-custom-controls', //String
-		customHtml: false, //String
+		container: '.cpr-custom-controls', // String
+		customHtml: false, // String
 		breakpoints: {
 			xs: {
-				opts: [1],
+				opts: [1, 2],
 				default: 1
 			},
 			sm: {
-				opts: [2],
+				opts: [2, 3],
 				default: 2
 			},
 			md: {
@@ -160,6 +253,5 @@ const cprGrid = new ColumnsPerRow({
 			}
 		}
 	}, // optional
-	transitions: false, // optional Bool
-	data: null // optional Array
+	transitions: false // optional Bool
 });
